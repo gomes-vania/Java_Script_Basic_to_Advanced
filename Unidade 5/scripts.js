537ee66aad9496e64daf277870164e96774a74fd
Vania Gomes - Let's Coding <vania.vgomes@mercadolivre.com>
2023-08-28 21:47:08 -0300

    Delete Unidade 5 directory

--- Unidade 5/scripts.js
+++
@@ -1,8 +0,0 @@
-// String
-console.log("Primeira linha\n Segunda linha");
-console.log("O meu nome é 'João'");
-console.log('O meu nome é "João"');
-console.log(`A divisão de 5 por 2 é ${5/2}`);
-console.log("O meu" + " nome é" + " João");
-
-
