6b6bb94f62ac718a1d76fd756e6d1ee9d16eed1d
Vania Gomes - Let's Coding <vania.vgomes@mercadolivre.com>
2023-08-28 21:46:31 -0300

    Delete Unidade 2 directory

--- Unidade 2/scripts.js
+++
@@ -1,8 +0,0 @@
-// Operações Aritméticas
-console.log(typeof (2 + 3));
-console.log(typeof (5 - 12));
-console.log(typeof (4 * 4));
-console.log(typeof (5 / 3));
-console.log(typeof (20 % 2));
-console.log(typeof (21 % 2));
-
