850c5f17084cdd99135f7773d293bbef3bdf9db2
Vania Gomes - Let's Coding <vania.vgomes@mercadolivre.com>
2023-08-28 21:46:56 -0300

    Delete Unidade 4 directory

--- Unidade 4/scripts.js
+++
@@ -1,8 +0,0 @@
-// String
-console.log(typeof "Hello World");
-console.log(typeof 'Hello World');
-console.log(typeof `Hello World`);
-console.log(typeof 'Infinity');
-console.log(typeof Infinity);
-
-
